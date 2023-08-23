53b9b3f4ee574bb5cfa7e825ceebfd91d1ba6fb1
ejkim000 <developer.ejk@gmail.com>
2023-08-23 11:10:44 -0400

    connected to mongodb

--- server.js
+++ server.js
@@ -1,88 +1,99 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 3006;
 const fruits = require("./models/fruits");
 const vegetables = require("./models/vegetables");
 
+// CONNECT WITH MONGOOSE
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+//   useCreateIndex: true, // don't use this anymore
+});
+
+mongoose.connection.once('open', () => {
+    console.log('connected to mongoDB');
+})
+
+
 // SETTING UP VIEW ENGINE
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
 // MIDDLEWARE
 app.use((req, res, next) => {
-    console.log('I run for all routes!')
-    next();
+  console.log("I run for all routes!");
+  next();
 });
 
 // this allows the body of post request
-app.use(express.urlencoded({extended:false})); // for the form submit
-
+app.use(express.urlencoded({ extended: true })); // for the form submit
 
 // ROUTES
-app.get('/fruits', (req, res) => {
-    // res.send(fruits);
-    res.render('fruits/Index', {
-        fruits: fruits
-    });
-})
-
+app.get("/fruits", (req, res) => {
+  // res.send(fruits);
+  res.render("fruits/Index", {
+    fruits: fruits,
+  });
+});
 
 // new fruit
-app.get('/fruits/new', (req, res) => {
-    res.render('fruits/New')
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/New");
 });
 
 // create = post
-app.post('/fruits', (req, res) => {
-    // set readyToEat value
-    (req.body.readyToEat === 'on') ? req.body.readyToEat = true : req.body.readyToEat = false;
-    // add new fruit to the fruits
-    fruits.push(req.body);
-    // redirect to the fruits list
-    res.redirect('/fruits');
+app.post("/fruits", (req, res) => {
+  // set readyToEat value
+  req.body.readyToEat === "on"
+    ? (req.body.readyToEat = true)
+    : (req.body.readyToEat = false);
+  // add new fruit to the fruits
+  fruits.push(req.body);
+  // redirect to the fruits list
+  res.redirect("/fruits");
 });
 
-
-app.get('/fruits/:index', (req, res) => {
-    // 1st param: Filename of view
-    // 2nd param: must be a object, variable available inside the jsx file
-    res.render('fruits/Show', {
-        fruit: fruits[req.params.index]
-    });
+app.get("/fruits/:index", (req, res) => {
+  // 1st param: Filename of view
+  // 2nd param: must be a object, variable available inside the jsx file
+  res.render("fruits/Show", {
+    fruit: fruits[req.params.index],
+  });
 });
 
-
-app.get('/vegetables', (req, res) => {
-    // res.send(fruits);
-    res.render('vegetables/Index', {
-        vegetables: vegetables
-    });
-})
-
-app.get('/vegetables/new', (req, res) => {
-    // res.send(fruits);
-    res.render('vegetables/New');
-})
-
-app.post('/vegetables', (req, res) => {
-    (req.body.readyToEat === 'on') ? req.body.readyToEat = true: req.body.readyToEat = false;
-
-    vegetables.push(req.body);
-
-    res.redirect('/vegetables');
-})
-
-app.get('/vegetables/:index', (req, res) => {
-    // 1st param: Filename of view
-    // 2nd param: must be a object, variable available inside the jsx file
-    res.render('vegetables/Show', {
-        vegetable: vegetables[req.params.index]
-    });
+app.get("/vegetables", (req, res) => {
+  // res.send(fruits);
+  res.render("vegetables/Index", {
+    vegetables: vegetables,
+  });
 });
 
+app.get("/vegetables/new", (req, res) => {
+  // res.send(fruits);
+  res.render("vegetables/New");
+});
 
+app.post("/vegetables", (req, res) => {
+  req.body.readyToEat === "on"
+    ? (req.body.readyToEat = true)
+    : (req.body.readyToEat = false);
 
+  vegetables.push(req.body);
+
+  res.redirect("/vegetables");
+});
+
+app.get("/vegetables/:index", (req, res) => {
+  // 1st param: Filename of view
+  // 2nd param: must be a object, variable available inside the jsx file
+  res.render("vegetables/Show", {
+    vegetable: vegetables[req.params.index],
+  });
+});
 
 // LISTEN
 app.listen(PORT, () => {
